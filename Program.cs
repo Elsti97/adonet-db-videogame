cb1b1ee168aa35d8fd309625b7511b8061d3d161
Elsti <Elsti.Shehati@hotmail.it>
2023-04-05 17:51:57 +0200

    Exercise done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,106 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using adonet_db_videogame;
+
+const string connStr = "Data Source = localhost; Initial Catalog = db - videogames; Integrated Security = True";
+var repo = new VideogameManager(connStr);
+
+bool exit = false;
+
+while (!exit)
+{
+    Console.WriteLine("Scegli una di queste opzioni:");
+    Console.WriteLine("inserire un nuovo videogioco (1)");
+    Console.WriteLine("ricercare un videogioco per id (2)");
+    Console.WriteLine("ricercare tutti i videogiochi aventi il nome contenente una determinata stringa inserita in input (3)");
+    Console.WriteLine("cancellare un videogioco (4)");
+    Console.WriteLine("chiudere il programma ('esci')");
+    string input = Console.ReadLine() ?? "";
+    Console.WriteLine();
+
+    switch (input)
+    {
+        case "1":
+            Inserisci();
+            break;
+        case "2":
+            RicercaID();
+            break;
+        case "3":
+            RicercaNome();
+            break;
+        case "4":
+            Elimina();
+            break;
+        case "esci":
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Errore, riprova");
+            break;
+    }
+}
+
+void Inserisci()
+{
+    Console.Write("Inserisci il nome: ");
+    var name = Console.ReadLine() ?? "";
+
+    Console.Write("Inserisci la descrizione: ");
+    var overview = Console.ReadLine() ?? "";
+
+    Console.Write("Inserisci la data d'uscita (yyyy-mm-dd): ");
+    var releaseDate = Convert.ToDateTime(Console.ReadLine());
+
+    Console.Write("Passa l'id della software house: ");
+    var softwareHouseId = Convert.ToInt64(Console.ReadLine());
+
+    var vg = new Videogame(0, name, overview, releaseDate, softwareHouseId);
+    var success = repo.InsertVideogame(vg);
+
+    if (success) Console.WriteLine("Videogioco inserito");
+    else Console.WriteLine("Inserimento fallito");
+}
+
+void RicercaID()
+{
+    Console.Write("Inserisci ID videogame: ");
+
+    var id = Convert.ToInt64(Console.ReadLine());
+    var vg = repo.GetById(id);
+
+    if (vg is null) Console.WriteLine("Videogame non trovato");
+    else Console.WriteLine(vg);
+}
+
+void RicercaNome()
+{
+    Console.Write("Inserisci nome videogame: ");
+
+    var name = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(name))
+    {
+        Console.WriteLine("Il nome del videogioco non può essere vuoto");
+        return;
+    }
+
+    var vg = repo.GetByName(name);
+
+    if (vg is null) Console.WriteLine("Videogame non trovato");
+    else Console.WriteLine("Videogame trovato: " + vg.ToString());
+}
+
+void Elimina()
+{
+    Console.Write("Inserisci ID videogame: ");
+
+    var id = Convert.ToInt64(Console.ReadLine());
+    var success = repo.DeleteVideogame(id);
+
+    if (success) Console.WriteLine("Videogioco eliminato");
+    else Console.WriteLine("Eliminazione fallita");
+}
+
+
+
---
+++ VideogameManager.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adonet_db_videogame;
+
+
+public class VideogameManager
+{
+    private string connStr;
+
+    public VideogameManager(string connStr)
+    {
+        this.connStr = connStr;
+    }
+
+    public Videogame? GetById(long id)
+    {
+        using var conn = new SqlConnection(connStr);
+
+        try
+        {
+            conn.Open();
+
+            using var cmd = new SqlCommand(VideogameQueries.SelectById, conn);
+            cmd.Parameters.AddWithValue("@Id", id);
+
+            var reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                var name = reader.GetString(1);
+                var overview = reader.GetString(2);
+                var releaseDate = reader.GetDateTime(3);
+                var softwareHouseId = reader.GetInt64(4);
+
+                return new Videogame(id, name, overview, releaseDate, softwareHouseId);
+            }
+
+            return null;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return null;
+        }
+    }
+
+    public List<Videogame> GetByName(string Name)
+    {
+        using var conn = new SqlConnection(connStr);
+
+        try
+        {
+            conn.Open();
+
+            using var cmd = new SqlCommand(VideogameQueries.SelectByName, conn);
+            cmd.Parameters.AddWithValue("@Name", "%" + Name + "%");
+
+            var reader = cmd.ExecuteReader();
+            var videogames = new List<Videogame>();
+
+            while (reader.Read())
+            {
+                var id = reader.GetInt64(0);
+                var name = reader.GetString(1);
+                var overview = reader.GetString(2);
+                var releaseDate = reader.GetDateTime(3);
+                var softwareHouseId = reader.GetInt64(4);
+
+                videogames.Add(new Videogame(id, name, overview, releaseDate, softwareHouseId));
+            }
+
+            return videogames;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return new List<Videogame>();
+        }
+    }
+
+
+    public bool InsertVideogame(Videogame videogame)
+    {
+        using var conn = new SqlConnection(connStr);
+
+        try
+        {
+            conn.Open();
+
+            using var cmd = new SqlCommand(VideogameQueries.Insert, conn);
+
+            cmd.Parameters.AddWithValue("@Name", videogame.Name);
+            cmd.Parameters.AddWithValue("@Overview", videogame.Overview);
+            cmd.Parameters.AddWithValue("@ReleaseDate", videogame.ReleaseDate);
+            cmd.Parameters.AddWithValue("@SoftwareHouseId", videogame.SoftwareHouseId);
+
+            cmd.ExecuteNonQuery();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    public bool DeleteVideogame(long id)
+    {
+        using var conn = new SqlConnection(connStr);
+
+        try
+        {
+            conn.Open();
+
+            using var cmd = new SqlCommand(VideogameQueries.DeleteById, conn);
+
+            cmd.Parameters.AddWithValue("@Id", id);
+
+            cmd.ExecuteNonQuery();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+    }
+}
+
+public static class VideogameQueries
+{
+    public const string Insert = "INSERT INTO videogames (name, overview, release_date, software_house_id) VALUES (@Name, @Overview, @ReleaseDate, @SoftwareHouseId)";
+    public const string SelectById = "SELECT id, name, overview, release_date, software_house_id FROM videogames WHERE Id = @Id";
+    public const string SelectByName = "SELECT id, name, overview, release_date, software_house_id FROM videogames WHERE Name LIKE @Name";
+    public const string DeleteById = "DELETE FROM videogames WHERE id = @Id";
+}
